3ff6547fb8d682f57b5e45c27dfc58e34a3849a8
FighterBisht <ssbsushil26@gmail.com>
2022-07-09 11:31:21 +0530

    Student.java Added

---
+++ Student.java
@@ -0,0 +1,24 @@
+class Hello{
+    String name;
+    int age;
+
+    void getInfo(){
+        System.out.println("Name : " + this.name);
+        System.out.println("Age : " + this.age);
+    }
+}
+
+
+public class Student{
+    public static void main(String[] args){
+        Hello h1 = new Hello();
+        h1.name = "Naveen";
+        h1.age = 21;
+        h1.getInfo();
+
+        Hello h2 = new Hello();
+        h2.name = "Sushil";
+        h2.age = 21;
+        h2.getInfo();
+    }
+}
