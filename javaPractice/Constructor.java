5755a8562d69a0f7d0c336d9e9bcf9e10f742f17
FighterBisht <ssbsushil26@gmail.com>
2022-07-09 11:57:31 +0530

    New Folder ceated for java practice

---
+++ javaPractice/Constructor.java
@@ -0,0 +1,17 @@
+package javaPractice;
+class Constructor{
+    String name;
+    int age;
+
+    Constructor(){
+        System.out.println("Constructor called");
+    }
+
+    void sushil(){
+        System.out.println("I am Sushil Bisht");
+    }
+    public static void main(String[] args){
+        Constructor obj = new Constructor();
+        obj.sushil();
+    }
+}
---
+++ javaPractice/Student.java
@@ -0,0 +1,30 @@
+package javaPractice;
+class Hello{
+    String name;
+    int age;
+
+    void getInfo(){
+        System.out.println("Name : " + this.name);
+        System.out.println("Age : " + this.age);
+    }
+}
+
+
+public class Student{
+    public static void main(String[] args){
+        Hello h1 = new Hello();
+        h1.name = "Naveen";
+        h1.age = 21;
+        h1.getInfo();
+
+        Hello h2 = new Hello();
+        h2.name = "Sushil";
+        h2.age = 21;
+        h2.getInfo();
+
+        Hello h3 = new Hello();
+        h3.name = "karan";
+        h3.age = 18;
+        h3.getInfo();
+    }
+}
