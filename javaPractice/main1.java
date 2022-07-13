ea3267fa93cb66efeb8122dcaac0c32a61632ff8
FighterBisht <ssbsushil26@gmail.com>
2022-07-13 21:55:41 +0530

    Conditional statement

---
+++ javaPractice/main1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class main1{
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+
+        if(button == 1){
+            System.out.println("namaste");
+        }else if(button == 2){
+            System.out.println("holla");
+        }else if(button == 3){
+            System.out.println("hi");
+        }else{
+            System.out.println("invalid button");
+        }
+    }
+        
+}
